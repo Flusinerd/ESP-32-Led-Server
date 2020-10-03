d2ac902e5f9add1734bf2d6722c7fbe592a85f92
Jan Krüger <jankrueger1999@gmail.com>
2020-10-03 18:44:52 +0200

    Init

---
+++ Esp32LED.ino
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include "secrets.h"
+#include "wifi.h"
+#define USE_SERIAL Serial
+
+void setup() {
+  setupWifi();
+}
+void loop(){
+  
+}
---
+++ wifi.h
@@ -0,0 +1,8 @@
+#include <WiFi.h>
+void setupWifi() {
+  WiFi.mode(WIFI_STA);
+  Serial.printf("Connecting to %s\n", WIFISSID);
+  if (String(WiFi.SSID()) != String(WIFISSID)) {
+    WiFi.begin(WIFISSID, WIFIPASS);
+  }
+}
